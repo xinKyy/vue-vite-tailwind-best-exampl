e0117fc6e6b694ee688cccdc93462149d952f858
xink <1611054604@qq.com>
2023-12-29 13:33:39 +0800

    fix:去掉本地代理配置，改成nginx反向代理

--- src/api/axios_instance.js
+++ src/api/axios_instance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: '/api', // 你的API的基本URL
+  baseURL: '/user_admin', // 你的API的基本URL
   timeout: 10000, // 请求超时时间
   headers: {
     'Content-Type': 'application/json',
